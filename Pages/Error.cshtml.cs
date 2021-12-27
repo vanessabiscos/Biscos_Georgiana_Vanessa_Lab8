6cbab194eac9ca06fb86a0dff621440feaeca5c3
vanessabiscos <92676814+vanessabiscos@users.noreply.github.com>
2021-12-27 15:43:15 +0200

    Update Error.cshtml.cs

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Biscos_Georgiana_Vanessa_Lab8.csproj.Pages
+namespace Biscos_Georgiana_Vanessa_Lab8.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]

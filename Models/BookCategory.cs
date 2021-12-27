57b2634a3a857a9ab701470d83c64ebfee98002c
vanessabiscos <92676814+vanessabiscos@users.noreply.github.com>
2021-12-27 15:38:18 +0200

    Create BookCategory.cs

---
+++ Models/BookCategory.cs
@@ -0,0 +1,11 @@
+namespace Biscos_Georgiana_Vanessa_Lab8.Models
+{
+    public class BookCategory
+    {
+        public int ID { get; set; }
+        public int BookID { get; set; }
+        public Book Book { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}

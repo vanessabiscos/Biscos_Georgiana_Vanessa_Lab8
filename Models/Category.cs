445e3e0b78815fc2954e856b57195198a5bcad6c
vanessabiscos <92676814+vanessabiscos@users.noreply.github.com>
2021-12-27 15:38:45 +0200

    Create Category.cs

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Biscos_Georgiana_Vanessa_Lab8.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+        public ICollection<BookCategory> BookCategories { get; set; }
+    }
+}

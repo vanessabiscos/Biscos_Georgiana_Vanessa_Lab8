dc1b29de3903ee502c4ad17537044c7df21f6779
vanessabiscos <92676814+vanessabiscos@users.noreply.github.com>
2021-12-27 16:42:06 +0200

    Create Create.cshtml.cs

---
+++ Pages/Publishers/Create.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Biscos_Georgiana_Vanessa_Lab8.Data;
+using Biscos_Georgiana_Vanessa_Lab8.Models;
+
+namespace Biscos_Georgiana_Vanessa_Lab8.Pages.Publishers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Biscos_Georgiana_Vanessa_Lab8.Data.Biscos_Georgiana_Vanessa_Lab8Context _context;
+
+        public CreateModel(Biscos_Georgiana_Vanessa_Lab8.Data.Biscos_Georgiana_Vanessa_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Publisher.Add(Publisher);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}

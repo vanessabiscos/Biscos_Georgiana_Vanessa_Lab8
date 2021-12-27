7b75f86559f1eb74bcdbad27ffb4bd5f57d996ee
vanessabiscos <92676814+vanessabiscos@users.noreply.github.com>
2021-12-27 16:33:49 +0200

    Create Delete.cshtml.cs

---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Biscos_Georgiana_Vanessa_Lab8.Data;
+using Biscos_Georgiana_Vanessa_Lab8.Models;
+
+namespace Biscos_Georgiana_Vanessa_Lab8.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Biscos_Georgiana_Vanessa_Lab8.Data.Biscos_Georgiana_Vanessa_Lab8Context _context;
+
+        public DeleteModel(Biscos_Georgiana_Vanessa_Lab8.Data.Biscos_Georgiana_Vanessa_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Category.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Category.FindAsync(id);
+
+            if (Category != null)
+            {
+                _context.Category.Remove(Category);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}

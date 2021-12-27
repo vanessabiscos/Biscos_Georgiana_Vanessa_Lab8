7499f645960c16fdd8b8c4b20a433551300582dc
vanessabiscos <92676814+vanessabiscos@users.noreply.github.com>
2021-12-27 15:39:16 +0200

    Create Publisher.cs

---
+++ Models/Publisher.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Biscos_Georgiana_Vanessa_Lab8.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}

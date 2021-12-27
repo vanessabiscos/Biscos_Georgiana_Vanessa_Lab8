f3d0500aed5bd7aa33e4d05ba174d6563f526028
vanessabiscos <92676814+vanessabiscos@users.noreply.github.com>
2021-12-27 15:49:41 +0200

    Create Create.cshtml.cs

---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Biscos_Georgiana_Vanessa_Lab8.Data;
+using Biscos_Georgiana_Vanessa_Lab8.Models;
+
+namespace Biscos_Georgiana_Vanessa_Lab8.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Biscos_Georgiana_Vanessa_Lab8.Data.Biscos_Georgiana_Vanessa_Lab8Context _context;
+
+        public CreateModel(Biscos_Georgiana_Vanessa_Lab8.Data.Biscos_Georgiana_Vanessa_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}

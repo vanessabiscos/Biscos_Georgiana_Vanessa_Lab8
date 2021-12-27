3e3148fe24ca20830a753cc4ec13e39b4bb31989
vanessabiscos <92676814+vanessabiscos@users.noreply.github.com>
2021-12-27 16:44:52 +0200

    Create Delete.cshtml.cs

---
+++ Pages/Publishers/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Biscos_Georgiana_Vanessa_Lab8.Data;
+using Biscos_Georgiana_Vanessa_Lab8.Models;
+
+namespace Biscos_Georgiana_Vanessa_Lab8.Pages.Publishers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Biscos_Georgiana_Vanessa_Lab8.Data.Biscos_Georgiana_Vanessa_Lab8Context _context;
+
+        public DeleteModel(Biscos_Georgiana_Vanessa_Lab8.Data.Biscos_Georgiana_Vanessa_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Publisher = await _context.Publisher.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Publisher == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Publisher = await _context.Publisher.FindAsync(id);
+
+            if (Publisher != null)
+            {
+                _context.Publisher.Remove(Publisher);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
